ecd0d8d558506062a7678553d39014a18c7677b3
xunfeng191 <48610962+xunfeng191@users.noreply.github.com>
2019-10-11 22:20:48 +0800

    Update my-notes.py

--- my-notes/my-notes.py
+++ my-notes/my-notes.py
@@ -1,2 +1,5 @@
-安装插件@jupyterlab/toc时出错，用一下命令安装nodejs
+1.安装插件@jupyterlab/toc时出错，用一下命令安装nodejs
 https://www.cnblogs.com/selfcs/p/11316390.html
+2.重新定向文件目录   
+    cd G:\GitHub\文档库\the-craft-of-selfteaching
+jupyter lab
